80f38b6d91aab5ec1be78cbd3a9f00ae8c011746
Invoker011 <atharvaborkar3182@gmail.com>
2025-07-01 16:36:56 -0400

    Problem 7 Solved

---
+++ find_index_of_first_occurence_in_the_string/Solution.java
@@ -0,0 +1,8 @@
+package find_index_of_first_occurence_in_the_string;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int index = haystack.indexOf(needle);
+        return index;
+    }
+}
