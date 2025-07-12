1af4883cce643cc59e086b64293f3f1b560afa85
Invoker011 <atharvaborkar3182@gmail.com>
2025-07-12 15:55:25 -0400

    Problem 33 solved

---
+++ Max_depth_of_tree/TreeNode.java
@@ -0,0 +1,15 @@
+package Max_depth_of_tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    
+}
