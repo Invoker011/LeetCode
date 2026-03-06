1290ab0b1de310c018390de463207a7c12a737a1
Invoker011 <atharvaborkar3182@gmail.com>
2026-03-06 13:20:09 -0500

    Problem 42 solved

---
+++ Max_Consecutive_ones_III/Solution.java
@@ -0,0 +1,43 @@
+package Max_Consecutive_ones_III;
+
+public class Solution {
+    public int maxConsecutiveOnes(int [] nums, int k ){
+        int maxLen =0, l=0,r=0,zeroCount=0;
+
+        while(r < nums.length){
+            if(nums[r] == 1){
+                maxLen = Math.max(maxLen, r - l + 1);
+                r++;
+            }
+            else if(nums[r] == 0){
+                zeroCount++;
+                if(zeroCount <= k){
+                    maxLen = Math.max(maxLen, r - l +1);
+                    r++;
+                }else{
+                    while(zeroCount != k){
+                        if(nums[l]==0){
+                            l++;
+                            zeroCount--;
+                            r++;
+                        }else{
+                            l++;
+                        }
+                       
+                    }
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args){
+        Solution sol =  new Solution();
+        int [] que = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+        int atMax = 3;
+        int result = sol.maxConsecutiveOnes(que,atMax);
+
+        System.out.println(result);
+    }
+    
+}
