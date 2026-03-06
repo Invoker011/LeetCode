65be793e13761e5caa4d3f255ab721b15a301652
Invoker011 <atharvaborkar3182@gmail.com>
2026-03-06 15:49:05 -0500

    Optimised Solution for 42

--- Max_Consecutive_ones_III/Solution.java
+++ Max_Consecutive_ones_III/Solution.java
@@ -30,6 +30,24 @@ public class Solution {
         }
         return maxLen;
     }
+    class Solution2 {
+    public int longestOnes(int[] nums, int k) {
+        int maxLen = 0;
+        int l = 0, r = 0;
+        int zeroCount = 0;
+
+        while(r < nums.length){
+            if(nums[r] == 0) zeroCount++;
+            if(zeroCount > k){
+                if(nums[l] == 0) zeroCount--;
+                l++;
+            }
+            if(zeroCount <= k) maxLen = Math.max(maxLen, r -l +1);
+            r++;
+        }
+        return maxLen;
+    }
+}
 
     public static void main(String[] args){
         Solution sol =  new Solution();
