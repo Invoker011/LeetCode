f6206ca14f851612d591185c9f95c1e031a38aeb
Invoker011 <atharvaborkar3182@gmail.com>
2026-04-07 23:14:39 -0400

    Problem 57 solved

---
+++ Lemonade_Change/Solution.java
@@ -0,0 +1,38 @@
+package Lemonade_Change;
+
+public class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+
+        for(int i =0; i < bills.length; i++){
+            if(bills[i] == 5) five++;
+
+            else if (bills[i] == 10){
+                if(five >=1){
+                    ten++;
+                    five--;
+                }else{
+                    return false;
+                }
+            }
+
+            else{
+                if(five >= 1 && ten >=1){
+                    twenty++;
+                    five--;
+                    ten--;
+                }
+                else if(five >= 3){
+                    five-=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+    
+}
