33f79423c6e196943d3087932215d4c38dd9fa34
Invoker011 <atharvaborkar3182@gmail.com>
2026-04-10 19:38:55 -0400

    Problem 64 solved

---
+++ String_To_Integer/Solution.java
@@ -0,0 +1,31 @@
+package String_To_Integer;
+
+public class Solution {
+    public int myAtoi(String s){
+        int i = 0;
+        int n = s.length();
+        if(s.length() == 0) return 0;
+        while(i < n && s.charAt(i) == ' ') i++;
+
+        int sign = 1;
+        if(i < n && (s.charAt(i)=='+' && s.charAt(i)=='-')){
+            if(s.charAt(i)=='-'){
+                sign = -1;
+            }
+            i++;
+        }
+        int result = 0;
+        while(i<n && Character.isDigit(s.charAt(i))){
+            int digit = s.charAt(i) - '0';
+
+            if(result > (Integer.MAX_VALUE - digit)/10){
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+        return result * sign;   
+    }
+    
+}
