4206b4e89b163bd8a0c28552fdba0e0009614b19
Invoker011 <atharvaborkar3182@gmail.com>
2026-07-16 17:04:55 -0400

    Problem 87 solved

---
+++ Bubble_sort/Bubble_Sort.java
@@ -0,0 +1,29 @@
+package Bubble_sort;
+
+public class Bubble_Sort {
+    public static int[] sort(int[] nums){
+        int n = nums.length;
+
+        for(int i = n - 1; i >=0; i--){
+            for(int j =0; j <= i-1; j++){
+                if(nums[j] > nums[j+1]){
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+
+    }
+    public static void main(String args[]){
+        int[] arr = {60, 54,80, 24,89,30};
+        int [] ans = Bubble_Sort.sort(arr);
+
+        for(int a : ans){
+            System.out.println(a);
+        }
+
+    }
+    
+}
